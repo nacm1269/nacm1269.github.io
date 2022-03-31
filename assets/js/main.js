706807cc3e4565a586bfa6a211bc57cb34145f67
nacm1269 <nacmuran@gmail.com>
2022-03-31 22:51:49 +0100

    tidied files and removed unused images

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,13 +42,7 @@ sr.reveal('.home__social-icon',{ interval: 200});
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
 sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
-
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.about__text',{delay: 400});
 
 /*SCROLL WORK*/
 sr.reveal('.work__img',{interval: 200}); 
